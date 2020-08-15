66761c74219730595f368600ad61836d8426b55c
rajharsh81070 <rajharsh81070@gmail.com>
2020-08-16 00:47:50 +0530

    Updates

---
+++ precache-manifest.16266369299f88d925e0451a03bb83b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359b36049745a2759cc6b28956d62a99",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "e4a64e0f13fe145e471c",
+    "url": "/My-Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a9ff8ef6646f3c9a4710",
+    "url": "/My-Portfolio/static/css/main.0f0fb17e.chunk.css"
+  },
+  {
+    "revision": "e4a64e0f13fe145e471c",
+    "url": "/My-Portfolio/static/js/2.b6d5b5c3.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/My-Portfolio/static/js/2.b6d5b5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ff8ef6646f3c9a4710",
+    "url": "/My-Portfolio/static/js/main.649ac0b7.chunk.js"
+  },
+  {
+    "revision": "cc8fb5c09ef196e1df09",
+    "url": "/My-Portfolio/static/js/runtime-main.2dca773a.js"
+  },
+  {
+    "revision": "102e56e2ee7edcea0d92f6906427af48",
+    "url": "/My-Portfolio/static/media/Harsh_Resume_31_07.102e56e2.pdf"
+  },
+  {
+    "revision": "15ae423622f1ae2af6f2a71c5e9d85ec",
+    "url": "/My-Portfolio/static/media/codechef.15ae4236.jpeg"
+  },
+  {
+    "revision": "bdfec5641d4c48b7290538f69fd7579e",
+    "url": "/My-Portfolio/static/media/udacity.bdfec564.svg"
+  }
+]);
